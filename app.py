64107e4615c9d29850d8341ce2b8adcb76a7b21e
aks2123 <44139244+aks2123@users.noreply.github.com>
2024-05-18 16:27:55 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,18 @@
+import streamlit as st
+st.title('My first app')
+a = st.number_input('Enter a number1')
+b = st.number_input('Enter a number2')
+#st.button('Add', key='add')
+#st.button('Subtract',key='subtract')
+#st.button('Multiply',key='multiply')
+#st.button('Divide',key='divide')
+
+if st.button('Add',key='add'):
+    st.success(a + b)
+if st.button('Subtract',key='subtract'):
+    st.success(a - b)
+if st.button('Multiply',key='multiply'):
+    st.success(a * b)
+if st.button('Divide',key='divide'):
+    st.success(a / b)
+    
